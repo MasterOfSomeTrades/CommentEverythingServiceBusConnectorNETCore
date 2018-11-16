7f681a5d4a16ea28a8d29469419342b676e7663d
MasterOfSomeTrades <amyas@rogers.com>
2018-11-16 04:28:15 -0500

    Remove max autorenew time

--- CommentEverythingServiceBusConnectorNETCore/Topic/SessionlessSubscriptionReceiver.cs
+++ CommentEverythingServiceBusConnectorNETCore/Topic/SessionlessSubscriptionReceiver.cs
@@ -70,8 +70,7 @@ namespace CommentEverythingServiceBusConnectorNETCore.Topic
 
             var sessionOptions = new MessageHandlerOptions(ExceptionReceivedHandler) {
                 AutoComplete = false,
-                MaxConcurrentCalls = _concurrentSessions,
-                MaxAutoRenewDuration = TimeSpan.FromSeconds(20)
+                MaxConcurrentCalls = _concurrentSessions
                 //MessageWaitTimeout = TimeSpan.FromSeconds(30)
             };
 
